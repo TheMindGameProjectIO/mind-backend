3fae4e73b02ab2f1a9bcfe06e78c8c260cda1219
MahammadNuriyev62 <maganuriyev@gmail.com>
2022-11-26 11:22:57 +0400

    compressing response

--- src/setups/rest.ts
+++ src/setups/rest.ts
@@ -1,44 +1,46 @@
 import express from "express";
 import cors from "cors";
-import session, { SessionOptions } from "express-session"
+import compression from "compression";
+import session, { SessionOptions } from "express-session";
 import { injectErrorDBHandlerToResponse, injectDefaultErrors } from "@/middlewares/error.middleware";
 import path from "path";
 import { fileURLToPath } from "url";
 import hbs from "@setups/view";
 const app = express();
+app.use(compression());
 
 var sess: SessionOptions = {
-    secret: 'keyboard cat',
+    secret: "keyboard cat",
     resave: false,
     saveUninitialized: true,
     cookie: {
         secure: false,
-    }
+    },
+};
+
+if (app.get("env") === "production") {
+    app.set("trust proxy", 1); // trust first proxy
+    sess.cookie.secure = true; // serve secure cookies
 }
 
-if (app.get('env') === 'production') {
-    app.set('trust proxy', 1) // trust first proxy
-    sess.cookie.secure = true // serve secure cookies
-}
-
-app.use(session(sess))
+app.use(session(sess));
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(cors({
-    origin: ['http://localhost:3000'],
-
-}));
+app.use(
+    cors({
+        origin: ["http://localhost:3000"],
+    })
+);
 app.use(express.json());
-app.use(injectErrorDBHandlerToResponse)
-app.use(injectDefaultErrors)
+app.use(injectErrorDBHandlerToResponse);
+app.use(injectDefaultErrors);
 
 // TEMPLATE ENGINE
-app.engine('handlebars', hbs.engine);
-app.set('view engine', "handlebars")
-app.set('views', path.join(__dirname, '../../public/views'))
-app.enable('view cache');
-
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+app.set("views", path.join(__dirname, "../../public/views"));
+app.enable("view cache");
 
 export default app;
