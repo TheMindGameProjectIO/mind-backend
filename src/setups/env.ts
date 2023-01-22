d7d63feade8653cab20f3785fc86d7143c505b94
MahammadNuriyev62 <maganuriyev@gmail.com>
2023-01-22 16:08:52 +0400

    docker-compose huge improvements

--- src/setups/env.ts
+++ src/setups/env.ts
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV === "production") {
     dotenv.config({ path: ".env.prod", override: true });
 } else {
     dotenv.config({ path: ".env.dev", override: true });
